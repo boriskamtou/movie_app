56943caf4e9bcec972fc7894b7fefe0d0625eab8
kamtou boris <boriskamtou@gmail.com>
2020-03-19 01:53:30 +0200

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:movie_app/widgets/large_button.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routeName = '/home-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Stack(
+              overflow: Overflow.visible,
+              children: <Widget>[
+                Container(
+                  height: 250,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(70),
+                      bottomRight: Radius.circular(70),
+                    ),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color(0xFFFF0700),
+                        Color(0xFFFF8800),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 120,
+                  left: MediaQuery.of(context).size.width / 2 - 125.5,
+                  child: Image.asset('assets/images/img1.png'),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              'Welcome',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xFF5D5D5D),
+                fontSize: 29,
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              'The interaction of the corporation &\n' +
+                  'the customer pushes the institutional\n' +
+                  'corporate identity',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+                color: Color(0xFFA29A9A),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            LargeButton(
+              text: 'SIGN IN',
+              onPressed: () {
+                //TODO:
+              },
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            LargeButton(
+              text: 'SIGN UP',
+              color: Color(0xFF8A8AFF),
+              onPressed: () {
+                //TODO:
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/large_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class LargeButton extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+  final Color color;
+
+  LargeButton({
+    @required this.text,
+    @required this.onPressed,
+    this.color = const Color(0xFFFF6600),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 32,
+      ),
+      child: RaisedButton(
+        onPressed: onPressed,
+        padding: EdgeInsets.symmetric(
+          vertical: 16,
+        ),
+        elevation: 0.75,
+        color: color,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
